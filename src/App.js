347ea2a5feb27f20b2f09e3d286b038a6210a6fc
stacy_r <stacyrileyca@gmail.com>
2023-09-20 13:06:07 -0500

    Updated notes on petprofiles and added id property

--- src/App.js
+++ src/App.js
@@ -2,90 +2,34 @@
 import React, { useState } from 'react';
 import './App.css';
 import database from './database.json';
+import PetCard from './petcard.js';
+import SearchBar from './searchbar.js';
 
 function App() {
-  //Create state variables to store the input values
-  const [searchValue, setSearchValue] = useState('');
-  const [locationValue, setLocationValue] = useState('');
+  //Create state variables 
+  const [filteredPets, setFilteredPets] = useState(database);
+  //This state controls card visibility at page load
+  const [showPetCard, setShowPetCard] = useState(false);
+  const handleSearch = (searchTerm) => {
+   
+    //test that it is working
+  // console.log('Search Term:', searchTerm);
+    const filtered = database.filter((pet) =>
+    pet.breed.toLowerCase().includes(searchTerm.toLowerCase()) 
+);
 
-//Move this outside of App() for better performance?
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  setFilteredPets(filtered);
+  setShowPetCard(true);
 
-    //This is the search and location values from state
-    console.log('Search Value:', searchValue);
-    console.log('Location Value:', locationValue);
-  }
-
+  //test that is is working
+  // console.log('Filtered Pets:', filtered);
+}
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <div>
-          { /*Search animal input field*/ }
-          <label htmlFor='searchInput'></label>
-          <input 
-            id='searchInput' 
-            placeholder='Search dog, cat, etc' 
-            type="text" 
-            className="searchInput"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
-          { /*Search location input field*/ }
-          <label htmlFor='searchLocation'></label>
-          {/* <input 
-            id='searchLocation' 
-            placeholder='Enter City Name' 
-            type="text" 
-            className="searchLocation"
-            value={locationValue}
-            onChange={(e) => setLocationValue(e.target.value)}
-          /> */}
-          <select 
-          name="searchLocation" 
-          id="searchLocation"
-          value={locationValue}
-          onChange={(e) => setLocationValue(e.target.value)}
-          >
-            <option value="">--Please choose an option--</option>
-            <option value="Quito">Quito</option>
-            <option value="Cuenca">Cuenca</option>
-            <option value="Guayaquil">Guayaquil</option>
-          </select>
-
-        </div>
-        <div>
-          <button type='submit'>Enter</button>
-        </div>
-      </form>
-
-      <div className='header-content-container'>
-        <h1>Find Your New Best Friend</h1>
-        <p>Browse pets from our network of selters and rescues</p>
-      </div>
-      <div>
-        {database.map(function(pet){
-          return (
-            <div className='pet-card' key={pet.id}>
-              <img 
-              //neither are working here-images not displaying
-                src={`./img/${pet.image}`}
-                //src="./img/{pet.image}" 
-                alt={pet.name}
-                width="200"
-                height="150"   
-              />
-              <h2>{pet.name}</h2>
-              <p>{pet.breed}</p>
-              <p>{pet.gender}</p>
-              <p>{pet.age}</p>
-              <p>{pet.location}</p>
-              <p>{pet.notes}</p>
-            </div> 
-          )
-          
-        })}
-      </div>
+    <div>
+      <h1>Look for your pet here!</h1>
+      <SearchBar onSearch={handleSearch}/>
+      {/* Conditionally render the PetCard Component */}
+      {showPetCard && <PetCard data={filteredPets} />}
     </div>
   );
 }
---
+++ src/searchbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function SearchBar( { onSearch }) {
+	const[searchTerm, setSearchTerm] = useState('');
+	const [locationValue, setLocationValue] = useState('')
+
+//Sets the state to be the entered data from the user
+	const handleInputChange = (e) => {
+		setSearchTerm(e.target.value);
+	}
+
+	// When button is clicked, onSearch function gives the search term that the user typed
+	const handleSearch = () => {
+		onSearch(searchTerm);
+	}
+
+	return (
+	<div className="form-container">
+		<form>
+			<div>
+			{ /*Search animal input field*/ }
+			<label htmlFor='searchInput'></label>
+			<input 
+				id='searchInput' 
+				placeholder='Search dog, cat, etc' 
+				type="text" 
+				className="searchInput"
+				value={searchTerm}
+				onChange={handleInputChange}
+			/>
+			
+			<select 
+				name="searchLocation" 
+				id="searchLocation"
+				value={locationValue}
+				onChange={(e) => setLocationValue(e.target.value)}
+			>
+				<option value="">--Please choose an option--</option>
+				<option value="Quito">Quito</option>
+				<option value="Cuenca">Cuenca</option>
+				<option value="Guayaquil">Guayaquil</option>
+			</select>
+
+			</div>
+			<div>
+				<button type='button' onClick={handleSearch}>Search</button>
+			</div>
+      </form>
+    </div>
+	)
+}
+
+export default SearchBar;
