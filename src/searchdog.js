9ff83306789e928738ab75cb75b2261e5f816b1b
stacy_r <stacyrileyca@gmail.com>
2023-09-25 15:38:32 -0500

    Added type input for dog and updated handleDogSearch to filter for different properties

--- src/searchdog.js
+++ src/searchdog.js
@@ -1,19 +1,53 @@
+//npm start
 import React, { useState } from 'react';
 
 function SearchDog ( { onClick }){
+	const [selectedDog, selectedDogSet] = useState('dog');
+	const [selectedAge, selectedAgeSet] = useState('');
+	const [selectedBreed, selectedBreedSet] = useState('');
+	const [selectedGender, selectedGenderSet] = useState('');
+	const [selectedSize, selectedSizeSet] = useState('');
+	//add more filter features here
 
-	const [ageValue, ageValueSet] = useState('');
-	const [genderValue, genderValueSet] = useState('');
+	const selectElementStyle = {
+		display: 'none',
+	}
 
 	//Sets the state to be the entered data from the user
+	//Uses the name to determine which state variable to update:
 	const handleChange = (e) => {
-		ageValueSet(e.target.value);
+		const {name, value} = e.target;
+
+		switch (name) {
+			case "selectedAge":
+				selectedAgeSet(value);
+				break;
+			case "selectedBreed":
+				selectedBreedSet(value);
+				break;
+			case "selectedGender":
+				selectedGenderSet(value);
+				break;
+			case "selectedSize":
+				selectedSizeSet(value);
+				break;
+			default:
+				break;
+		}
 	}
-	//When search is clicked the new state is recorded:
+	
 	const handleDogSearch = () => {
-		onClick(ageValue);
-		
-		// genderValueSet(e.target.value);
+	// Collect all selected filter values and pass them to the onClick function:
+		const filters = {
+			age: selectedAge,
+			breed: selectedBreed,
+			gender: selectedGender,
+			size: selectedSize,
+			type: selectedDog,
+		  };
+
+		onClick(filters);
+
 	}
 
 	return (
@@ -27,14 +61,41 @@ function SearchDog ( { onClick }){
 								<p>It only takes 60 seconds!</p>
 							</div>
 							<div>
-							{ /*Search animal input field*/ }
-							<label htmlFor='ageSelect'>AGE</label>
+							{/* Selected dog as type - not to be displayed on screen */}
+							<label htmlFor='selectedDog' style={selectElementStyle}></label>
+							<select
+								name="selectedDog"
+								id="selectedDog"
+								style={selectElementStyle}
+								value={selectedDog}
+								onChange={(e) => selectedDogSet(e.target.value)}
+							>
+								<option value="dog"></option>
+							</select>
+							{ /*Select breed field*/ }
+							<label htmlFor='selectedBreed'>Breed</label>
 							<br/>
 							<select 
-								name="ageSelect" 
-								id="ageSelect"
-								value={ageValue}
-								onChange={(e) => ageValueSet(e.target.value)}
+								name="selectedBreed" 
+								id="selectedBreed"
+								value={selectedBreed}
+								onChange={(e) => selectedBreedSet(e.target.value)}
+							>
+								<option value="any">Any</option>
+								<option value="chihuahua">Chihuahua</option>
+								<option value="german shepherd">German Shepherd</option>
+								<option value="shit tzu">Shit Tzu</option>
+								<option value="terrier">Terrier</option>
+							</select>
+							<br/>
+							{ /*Select age field*/ }
+							<label htmlFor='selectedAge'>Age</label>
+							<br/>
+							<select 
+								name="selectedAge" 
+								id="selectedAge"
+								value={selectedAge}
+								onChange={(e) => selectedAgeSet(e.target.value)}
 							>
 								<option value="any">Any</option>
 								<option value="puppy">Puppy</option>
@@ -43,17 +104,34 @@ function SearchDog ( { onClick }){
 								<option value="senior">Senior</option>
 							</select>
 							<br/>
-							<label htmlFor='genderSelect'>Gender</label>
+							{ /*Select gender field*/ }
+							<label htmlFor='selectedGender'>Gender</label>
 							<br/>
 							<select 
-								name="genderSelect" 
-								id="genderSelect"
-								value={genderValue}
-								onChange={(e) => genderValueSet(e.target.value)}
+								name="selectedGender" 
+								id="selectedGender"
+								value={selectedGender}
+								onChange={(e) => selectedGenderSet(e.target.value)}
 							>
 								<option value="any">Any</option>
 								<option value="male">Male</option>
 								<option value="female">Female</option>
+							</select>
+							<br/>
+							{ /*Select size field*/ }
+							<label htmlFor='selectedSize'>Size</label>
+							<br/>
+							<select 
+								name="selectedSize" 
+								id="selectedSize"
+								value={selectedSize}
+								onChange={(e) => selectedSizeSet(e.target.value)}
+							>
+								<option value="any">Any</option>
+								<option value="small">Small (0-25lbs)</option>
+								<option value="medium">Medium (26-60lbs)</option>
+								<option value="large">Large (61-100lbs)</option>
+								<option value="xlarge">Extra Large (101lbs or more)</option>
 							</select>
 							</div>
 							<br/>
