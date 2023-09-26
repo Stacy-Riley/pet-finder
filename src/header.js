757109f5f5010f5a6ea36fe06441d204899be432
stacy_r <stacyrileyca@gmail.com>
2023-09-26 17:05:31 -0500

    Initial commit for header component

---
+++ src/header.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './header.css';
+import Navigation from './navigation.js';
+
+function Header() {
+
+	return (
+		<div className='header'>
+			<div className='header__wrapper'>
+				<div className='header__container'>
+					<div className='header__container__logo'>
+						<p>petfinder</p>	
+					</div>
+					<div className='header__container__favIcon'>
+						<span class="material-symbols-rounded">favorite</span>
+					</div>
+					<div className='header__container__nav'>
+						<Navigation/>
+					</div>
+				</div>			
+			</div>
+		</div>
+
+	)
+}
+
+export default Header;
