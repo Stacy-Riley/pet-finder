127d8e9cdd2913b3d8b6d47f800bc2159b74b8d6
stacy_r <stacyrileyca@gmail.com>
2023-11-10 13:50:23 -0500

    Removed outdated code and added notes

--- src/searchdog.js
+++ src/searchdog.js
@@ -1,17 +1,16 @@
-//npm start
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid'; //library to generate unique keys 
 import './searchform.css';
 
 
 function SearchDog ( { onClick }){
-	// const [selectedDog, selectedDogSet] = useState('dog'); //[] destructures
 	const [selectedAge, selectedAgeSet] = useState('');
 	const [selectedBreed, selectedBreedSet] = useState('');
 	const [selectedGender, selectedGenderSet] = useState('');
 	const [selectedSize, selectedSizeSet] = useState('');
 	const [showDogCard, setShowDogCard] = useState(false);
 
+	//Collects the state of all the form elements
 	const [filters, setFilters] = useState({
 		age: selectedAge,
 		breed: selectedBreed,
@@ -19,59 +18,8 @@ function SearchDog ( { onClick }){
 		size: selectedSize,
 		type: 'dog',
 	});
-	
 
-	// Selected dog as type - not to be displayed on screen
-	const selectElementStyle = {
-		display: 'none',
-	}
-	
-	const buttonSpacing = {
-		margin: '0 5px 10px 0',
-		// background: 'none',
-	}
-	
-	//Triggered when the search button is clicked and collects
-	//the selected filter values, creates a 'filters' object and
-	//passes it to the 'onClick' function provided as a prop:
-	const handleDogSearch = () => {
-		// const filters = {
-		// 	age: selectedAge,
-		// 	breed: selectedBreed,
-		// 	gender: selectedGender,
-		// 	size: selectedSize,
-		// 	type: selectedDog,
-		//   };
-	
-		
-console.log("filters from SearchDog", filters)
-// console.log('searchTerms list:', searchTerms)
-
-	// console.log("filters in handleDogSearch - component", filters)  
-	// console.log("filters type:", filters.type)
-	onClick(filters);
-		  
-
-		
-	}
-
-//Removes the user selected inputs from screen under search button
-	const handleRemoveSearchTerm = (valueToRemove) => {
-		// const updatedSearchTerms = filters.filter(function(value) {
-		// 	//not equal in value and type
-		// 	if(value !== valueToRemove){
-		// 		return true; // Include the element in the filtered array because there wasn't a match
-		// 	} else {
-		// 		return false; // Exclude the element in the filtered array because there was a match
-		// 	}
-
-		// });
-		// setFilters(updatedSearchTerms);
-		// console.log("Updated Search Terms from updatedSearchTerms", updatedSearchTerms)
-	}
-
-
-	//Update filters whenever any of the individual state variables change
+	//Updates filters whenever any of the individual state variables change
 	useEffect(() => {
 		setFilters({
 		  age: selectedAge,
@@ -82,6 +30,50 @@ console.log("filters from SearchDog", filters)
 		});
 	  }, [selectedAge, selectedBreed, selectedGender, selectedSize, 'dog']);
 
+	
+	//Triggered when the search button is clicked 
+	//passes filters to the 'onClick' function provided as a prop to App.js:
+	const handleDogSearch = () => {
+		
+		onClick(filters);
+		  
+	}
+
+// //Removes the user selected inputs from screen under search button
+// 	const handleRemoveSearchTerm = (valueToRemove) => {
+// 		const filterNew = Object.values(filters)
+// 		const updatedSearchTerms = filterNew.filter(function(value) {
+// 		// 	//not equal in value and type
+// 			if(value !== valueToRemove){
+// 				return true; // Include the element in the filtered array because there wasn't a match
+// 			} else {
+// 				return false; // Exclude the element in the filtered array because there was a match
+// 			}
+
+// 		});
+// 		setFilters(updatedSearchTerms);
+// 		console.log("Updated Search Terms from updatedSearchTerms", updatedSearchTerms)
+// 	}
+
+// //Removes the user selected inputs from screen under search button
+const handleRemoveSearchTerm = (valueToRemove) => {
+	const updatedSearchTerms = Object.fromEntries(
+	  Object.entries(filters).map(([key, value]) => [
+		key,
+		value === valueToRemove ? '' : value,
+	  ])
+	);
+  
+	setFilters(updatedSearchTerms);
+	console.log("Updated Search Terms:", updatedSearchTerms);
+  };
+  
+//Button style for handleRemoveSearchTerm
+  const buttonSpacing = {
+	margin: '0 5px 10px 0',
+	// background: 'none',
+	}
+
 	return (
 			<div className='search-wrapper'>
 				<div className='search-container'>
@@ -91,31 +83,18 @@ console.log("filters from SearchDog", filters)
 							<p>It only takes 60 seconds!</p>
 						</div>
 						<div className='form-input-container'>
-						{/* Selected dog as type - not to be displayed on screen */}
-						<label htmlFor='selectedDog' 
-						// style={selectElementStyle}
-						>
-
-						</label>
-						{/* <select
-							name="selectedDog"
-							id="selectedDog"
-							style={selectElementStyle}
-							defaultValue={selectedDog}
-							// onChange={(e) => selectedDogSet(e.target.value)}
-						>
-							{/* <option value="dog"></option> */}
-						{/* </select> } */}
-						{ /*Select breed field*/ }
-						<label htmlFor='selectedBreed'>Breed</label>
+						{/* Selected dog as type - not to be displayed on screen
+						static value inside of filters state under type: 'dog' */}
 						
+						<label htmlFor='selectedBreed'>Breed</label>
+
 						<select 
 							name="selectedBreed" 
 							id="selectedBreed"
 							value={selectedBreed}
 							onChange={(e) => selectedBreedSet(e.target.value)}
 						>
-							<option value="any">Any</option>
+							<option value="">Any</option>
 							<option value="chihuahua">Chihuahua</option>
 							<option value="german shepherd">German Shepherd</option>
 							<option value="shih tzu">Shit Tzu</option>
@@ -131,7 +110,7 @@ console.log("filters from SearchDog", filters)
 							value={selectedAge}
 							onChange={(e) => selectedAgeSet(e.target.value)}
 						>
-							<option value="any">Any</option>
+							<option value="">Any</option>
 							<option value="puppy">Puppy</option>
 							<option value="young">Young</option>
 							<option value="adult">Adult</option>
@@ -146,7 +125,7 @@ console.log("filters from SearchDog", filters)
 							value={selectedGender}
 							onChange={(e) => selectedGenderSet(e.target.value)}
 						>
-							<option value="any">Any</option>
+							<option value="">Any</option>
 							<option value="male">Male</option>
 							<option value="female">Female</option>
 						</select>
@@ -160,7 +139,7 @@ console.log("filters from SearchDog", filters)
 							value={selectedSize}
 							onChange={(e) => selectedSizeSet(e.target.value)}
 						>
-							<option value="any">Any</option>
+							<option value="">Any</option>
 							<option value="small">Small (0-25lbs)</option>
 							<option value="medium">Medium (26-60lbs)</option>
 							<option value="large">Large (61-100lbs)</option>
@@ -172,7 +151,9 @@ console.log("filters from SearchDog", filters)
 							<button type='button' onClick={handleDogSearch}>Search</button>
 						</div>
 						{/* Map over the user selected inputs and display on the screen */}
-							{/* <div>
+						
+					</form>
+					<div>
 								{Object.entries(filters).map(([key, value]) => (
 									
 									//gave each term a unique key by importing from library uuid
@@ -184,8 +165,7 @@ console.log("filters from SearchDog", filters)
 										{value}
 									</div>) : null
 								))}
-							</div> */}
-					</form>
+							</div>
 				</div>
 			</div>
 	)
