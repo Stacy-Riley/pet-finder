fec7989aab4f20810fb4a3e3902be235b531ed98
stacy_r <stacyrileyca@gmail.com>
2023-11-16 16:11:30 -0500

    Added styles to the searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import { BiSearchAlt } from "react-icons/bi";
 import '../styles/searchbar.css';
 
 function SearchBar( { isVisible, onSearch }) {
 	const[searchTerm, setSearchTerm] = useState('');
-	// const [locationValue, setLocationValue] = useState('')
+	const [searchLocation, setSearchLocation] = useState('')
 
 	
 	//Sets the state to be the entered data from the user
@@ -25,32 +26,44 @@ function SearchBar( { isVisible, onSearch }) {
 				</div>
 				<div className='searchbar__container__form'>
 					<form onSubmit={handleSearch}>
-						<div>
+						<div className='searchbar__form__container'>
 						{ /*Search animal input field*/ }
 						<label htmlFor='searchInput'></label>
 						<input 
 							id='searchInput' 
 							placeholder='Search Terrier, Dog, Cat, etc' 
 							type="text" 
-							className="searchInput"
+							className='searchInput'
 							value={searchTerm}
 							onChange={(e) => setSearchTerm(e.target.value)}
 						/>
-						{/* Saving location search for another part of app for now */}
+						{/* <p><span className='searchDivide'>|</span></p> */}
+						<label htmlFor='searchLocation'><span className='searchDivide'>|</span></label>
+						<input
+							id='searchLocation'
+							placeholder='City, State'
+							type='text'
+							className='searchLocation'
+							value={searchLocation}
+							onChange={(e) => setSearchLocation(e.target.value)}
+						/>
+						{/* Saving location search for another part of app for now  */}
 						{/* <select 
 							name="searchLocation" 
 							id="searchLocation"
 							value={locationValue}
 							onChange={(e) => setLocationValue(e.target.value)}
 						>
-							<option value="">--Please choose an option--</option>
+							<option>--Choose a City--</option>
+							<option value="">Any</option>
 							<option value="Quito">Quito</option>
 							<option value="Cuenca">Cuenca</option>
 							<option value="Guayaquil">Guayaquil</option>
 						</select> */}
+						
 						</div>
-						<div className='searchbar__container__form__button'>
-							<button type='submit'>Search</button>
+							<div className='searchbar__container__form__button'>
+							<button type='submit'><BiSearchAlt /></button>
 						
 						</div>
 					</form>
