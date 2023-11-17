8f3456d2377f42be9496917fed64be08ae63bac9
stacy_r <stacyrileyca@gmail.com>
2023-11-17 16:16:51 -0500

    Cleaned up code

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -27,45 +27,43 @@ function SearchBar( { isVisible, onSearch }) {
 				<div className='searchbar__container__form'>
 					<form onSubmit={handleSearch}>
 						<div className='searchbar__form__container'>
-						{ /*Search animal input field*/ }
-						<label htmlFor='searchInput'></label>
-						<input 
-							id='searchInput' 
-							placeholder='Search Terrier, Dog, Cat, etc' 
-							type="text" 
-							className='searchInput'
-							value={searchTerm}
-							onChange={(e) => setSearchTerm(e.target.value)}
-						/>
-						{/* <p><span className='searchDivide'>|</span></p> */}
-						<label htmlFor='searchLocation'><span className='searchDivide'>|</span></label>
-						<input
-							id='searchLocation'
-							placeholder='City, State'
-							type='text'
-							className='searchLocation'
-							value={searchLocation}
-							onChange={(e) => setSearchLocation(e.target.value)}
-						/>
-						{/* Saving location search for another part of app for now  */}
-						{/* <select 
-							name="searchLocation" 
-							id="searchLocation"
-							value={locationValue}
-							onChange={(e) => setLocationValue(e.target.value)}
-						>
-							<option>--Choose a City--</option>
-							<option value="">Any</option>
-							<option value="Quito">Quito</option>
-							<option value="Cuenca">Cuenca</option>
-							<option value="Guayaquil">Guayaquil</option>
-						</select> */}
-						
+							{ /*Search animal input field*/ }
+							<label htmlFor='searchInput'></label>
+							<input 
+								id='searchInput' 
+								placeholder='Search Terrier, Dog, Cat, etc' 
+								type="text" 
+								className='searchInput'
+								value={searchTerm}
+								onChange={(e) => setSearchTerm(e.target.value)}
+							/>
+		
+							<label htmlFor='searchLocation'><span className='searchDivide'>|</span></label>
+							<input
+								id='searchLocation'
+								placeholder='City, State'
+								type='text'
+								className='searchLocation'
+								value={searchLocation}
+								onChange={(e) => setSearchLocation(e.target.value)}
+							/>
+									{/* Saving location search for another part of app for now  */}
+									{/* <select 
+										name="searchLocation" 
+										id="searchLocation"
+										value={locationValue}
+										onChange={(e) => setLocationValue(e.target.value)}
+									>
+										<option>--Choose a City--</option>
+										<option value="">Any</option>
+										<option value="Quito">Quito</option>
+										<option value="Cuenca">Cuenca</option>
+										<option value="Guayaquil">Guayaquil</option>
+									</select> */}
 						</div>
 							<div className='searchbar__container__form__button'>
-							<button type='submit'><BiSearchAlt /></button>
-						
-						</div>
+								<button type='submit'><BiSearchAlt /></button>
+							</div>
 					</form>
 				</div>
 			</div>
