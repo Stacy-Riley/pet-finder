c90a2d4d2ac7f7a02e916f8725003298ebd9cea6
stacy_r <stacyrileyca@gmail.com>
2023-12-11 11:55:00 -0500

    Imported the routes and route components from react-router-dom and created a home component moving the searchbar and searchdog components there

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-//constructor function is uppercase
-import React, { useState } from 'react';
-import database from './assets/data/database.json';
-import PetCard from './components/PetCard.js';
-import SearchBar from './components/SearchBar.js';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home.js";
 import Header from './components/Header.js';
-import ImageCardDisplay from './components/ImageCardDisplay.js';
+import SearchDog from "./components/SearchDog.js";
 import './App.css';
-import './styles/cardLink.css';
 
 
 function App() {
-  //Create state variables 
-  const [filteredPets, setFilteredPets] = useState(database);
-  //This state controls card visibility at page load
-  const [showPetCard, setShowPetCard] = useState(false);
-  //This state controls searchBar in header visibility:
-  const [isSearchBarVisible, setSearchBarVisibility] = useState(true);
-
-  //Used for searchbar.js
-  const handleSearch = (searchTerm) => {
-    //test that it is working
-  // console.log('Search Term:', searchTerm);
-    const filtered = database.filter((pet) => {
-      const breedMatch = searchTerm.toLowerCase() === pet.breed.toLowerCase();
-      const typeMatch = searchTerm.toLowerCase() === pet.type.toLowerCase();
-    //Only return items that match all the above properties:
-      return breedMatch || typeMatch;
-  })
-
-  setFilteredPets(filtered);
-  setShowPetCard(true);
-}
-
-const toggleSearchBarVisibility = () => {
-  setSearchBarVisibility(false);
-  //If there were cards displayed in the first searchbar and then an Image card was pressed
-  //this will hide the card 
-  setShowPetCard(false);
-}
-
-  return (
-    <div>
+  
+  return ( 
       <div className='app-wrapper'>
         <div className='app-container'>
-          <Header/>
-          <SearchBar 
-            onSearch={handleSearch}
-            isVisible={isSearchBarVisible}
-          />
-            {/* Conditionally render the PetCard Component */}
-            {showPetCard && <PetCard data={filteredPets} />}
+          <Header/>  
         </div>
-        <div>
-          {/* Section that shows imagecards that when clicked on open form */}
-          <ImageCardDisplay
-            onImageClick={toggleSearchBarVisibility}
-          />
-        </div>
+        <Routes>
+            <Route path="/" element= {<Home />} />
+            <Route path="/searchdog" element= { <SearchDog /> } />
+        </Routes>
+
       </div>
-    </div>
+
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import database from '../assets/data/database.json';
+import PetCard from '../components/PetCard.js';
+import SearchBar from '../components/SearchBar.js';
+import ImageCardDisplay from '../components/ImageCardDisplay.js';
+import '../App.css';
+import '../styles/cardLink.css';
+
+function Home(){
+//Create state variables 
+const [filteredPets, setFilteredPets] = useState(database);
+//This state controls card visibility at page load
+const [showPetCard, setShowPetCard] = useState(false);
+//This state controls searchBar in header visibility:
+const [isSearchBarVisible, setSearchBarVisibility] = useState(true);
+
+//Used for searchbar.js
+const handleSearch = (searchTerm) => {
+  //test that it is working
+// console.log('Search Term:', searchTerm);
+  const filtered = database.filter((pet) => {
+	const breedMatch = searchTerm.toLowerCase() === pet.breed.toLowerCase();
+	const typeMatch = searchTerm.toLowerCase() === pet.type.toLowerCase();
+  //Only return items that match all the above properties:
+	return breedMatch || typeMatch;
+})
+
+setFilteredPets(filtered);
+setShowPetCard(true);
+}
+
+const toggleSearchBarVisibility = () => {
+setSearchBarVisibility(false);
+//If there were cards displayed in the first searchbar and then an Image card was pressed
+//this will hide the card 
+setShowPetCard(false);
+}
+	return (
+		<>
+			{/* First Searchbar */}
+			<div>
+				<SearchBar 
+					onSearch={handleSearch}
+					isVisible={isSearchBarVisible}
+				/>
+			</div>
+
+			<div>
+				{/* Conditionally render the PetCard Component */}
+				{showPetCard && <PetCard data={filteredPets} />}
+			</div>
+            
+
+			<div>
+				{/* Section that shows imagecards that when clicked on open form */}
+				<ImageCardDisplay
+					onImageClick={toggleSearchBarVisibility}
+				/>
+        	</div>
+		</>
+	)
+}
+
+
+
+
+
+
+export default Home;
