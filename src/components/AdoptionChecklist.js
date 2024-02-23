3a3c99af7ea88a402d1a13f1ef3f47f2dc1e29ab
stacy_r <stacyrileyca@gmail.com>
2024-02-23 14:16:50 -0500

    Added adoption checklist data to the page

--- src/components/AdoptionChecklist.js
+++ src/components/AdoptionChecklist.js
@@ -1,11 +1,320 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../App.css';
 
 function AdoptionChecklist(){
 
 	return (
 		<>
-			
+			<div>
+				<div className="adoption-checklist-container-1">
+					<figure>
+					<img src={require(`../assets/icons/paw-2-svgrepo-com.svg`).default} alt="paw icon"/>
+					</figure>
+					<p>
+					Well done on your pet adoption! You are starting a fantastic and fulfilling relationship. 
+					We've created a checklist to help ensure that things go smoothly because adopting a new pet 
+					involves many changes for both the pet and the pet parent.
+					</p>
+					<p>
+					<strong>Questions For All Adopters:</strong>
+					</p>
+					<ul>
+						<li>What kind of reactions might your existing pets have to a new addition?
+							Is the pet you're thinking about a good fit for your present home?
+						</li>
+						<li>
+						How will your duties at work or in your personal life impact your capacity to take care of a pet?
+						</li>
+						<li>
+						Do you have a strategy in place for your new pet while you travel for business or vacation?
+						</li>
+						<li>
+						What are your roommates' thoughts on having a pet in the house?
+						</li>
+						<li>
+						Are you intolerant of hair, grime, and other aspects of living with a pet in your home, such allergies, or are you and your partner, spouse, or roommate?
+						</li>
+						<li>
+						Do you or anybody else in your home/family have any health conditions that a pet could exacerbate?
+						</li>
+						<li>
+						Which type of animal or breed best suits your way of life at this time?
+						</li>
+						<li>
+						Does your household have tension? Stress in the house can rapidly be picked up by pets, and it can worsen their behavioral and health issues.
+						</li>
+						<li>
+						Does someone in the household have a written agreement to take ultimate responsibility for the pet's care?
+						</li>
+					</ul>
+				</div>
+				<div className="adoption-checklist-container-2">
+					<p><strong>Other Considerations:</strong></p>
+					<ul>
+						<li>
+						What positive effects do you hope your pet will have on your life? For instance, is your idea of exercise watching it on TV, or do you prefer a jogging and hiking partner?
+						</li>
+						<li>
+						If you're considering adopting a young animal, are you willing to invest the time and patience necessary to help the pet through puberty, taking into consideration issues like activity levels, chewing, and housebreaking?
+						</li>
+						<li>
+						Have you given great thought to your lifestyle and decided if an older or younger animal will suit you better?
+						</li>
+						<li>
+						Are you searching for a laid-back friend, or are you able to train and manage a pet with behavioral issues?
+						</li>
+						<li>
+						Do you need a pet that you can travel with or one that is dependable around kids?
+						</li>
+						<li>
+						Would you rather have a less needy, more independent pet or one that follows you around the house?
+						</li>
+					</ul>
+				</div>
+				<div className="adoption-checklist-container-3">
+					<p><strong>Size Considerations (for Dogs):</strong></p>
+					<ul>
+						<li>
+						What size dog can you fit in your living space?
+						</li>
+						<li>
+						If your dog ends up becoming larger than you anticipated, will you have enough space?
+						</li>
+						<li>
+						Which size pet would be appropriate for the other residents or guests who frequently visit your home?
+						</li>
+						<li>
+						When deciding on the size of your next pet, is there anything else you need to think about?
+						</li>
+						<li>
+						What size pet can you bring on a trip without discomfort?
+						</li>
+					</ul>
+				</div>
+				<div className="adoption-checklist-container-3">
+					<p><strong>Pet Costs:</strong></p>
+					<ul>
+						<li>
+						The adopting organization will almost certainly charge a fee to cover part of the expenses associated with taking in unwanted or lost animals. The amount of money you will spend on your pet during their lifetime will be much larger than the adoption fee you pay.
+						</li>
+						<li>
+						Before bringing your adopted pet home, you may be required to pay to have him or her spayed or neutered.
+						</li>
+						<li>
+						All pets must incur certain costs, such as:
+						</li>
+						<ul>
+							<li>
+							Food
+							</li>
+							<li>
+							Periodic veterinary treatment
+							</li>
+							<li>
+							Licensing in compliance with regional laws
+							</li>
+							<li>
+							Leashes, collars, and identity tags
+							</li>
+							<li>
+							Litter box for cats
+							</li>
+							<li>
+							Basic supplies and tools for grooming.
+							</li>
+							<li>
+							Although they might not be necessary, the following expenses are strongly suggested:
+							</li>
+							<ul>
+								<li>
+								Permanent identification, such a tattoo or microchip implant
+								</li>
+								<li>
+								Training sessions
+								</li>
+								<li>
+								Depending on the requirements of your new pet, extra grooming materials or expert grooming
+								</li>
+								<li>
+								An extra leash or collar
+								</li>
+								<li>
+								Toys and a bed
+								</li>
+								<li>
+								A carrier or crate
+								</li>
+							</ul>
+							<li>
+							Unexpected expenses: Emergency veterinary care can be expensive in the event of a sickness or accident. Posters and rewards are examples of recovery tools that can help locate a missing pet.
+							</li>
+							<li>
+							Specialized expert assistance may be necessary for a pet with unique behavioral or physical disabilities to overcome any challenges these issues may offer.
+							</li>
+						</ul>
+					</ul>
+				</div>
+				<div className="adoption-checklist-container-4">
+					<p><strong>Time Considerations:</strong></p>
+					<ul>
+						<li>
+						Pets require two to three meals a day, or more frequently if they are puppies, as well as access to fresh water at all times.
+						</li>
+						<li>
+						A responsible pet owner should provide their pet with direct attention for at least one hour each day. This could involve playing, exercising, grooming, training, or, in the case of cats, just lounging on the couch for lap time. Dogs will require multiple daily trips outside to relieve themselves.
+						</li>
+						<li>
+						An overly active pet requires more time for exercise and engaging toys to keep them occupied.
+						</li>
+						<li>
+						To avoid matting, pets with long coats require 20 minutes of daily brushing.
+						</li>
+						<li>
+						Certain medical disorders in pets may require extra care; for example, diabetic animals may require precisely timed injections.
+						</li>
+						<li>
+						Keep in mind that in the initial weeks, adopted pets could require more time for bonding and trust.
+						</li>
+					</ul>
+				</div>
+				<div className="adoption-checklist-container-5">
+					<p><strong>Shopping Checklist:</strong></p>
+					<p>
+					It might be a good idea to put off starting your supply buying until after you've chosen your new pet. Certain things, like collars and 
+					harnesses or bowls for food and water, for instance, are dependent on the size of the pet you plan to adopt.
+					</p>
+					<p>
+					To further facilitate the transition, find out what food your pet was fed in the shelter or foster home so that you may start with the same supplies. 
+					Consult your veterinarian about introducing your preferred diet once the pet has been used to its new surroundings.
+					</p>
+					<p>
+					Following your pet selection, this list of goods may be necessary:
+					</p>
+					<ul>
+					<li>
+					<strong>Necessary Items for Dogs:</strong>
+					</li>
+						<ul>
+							<li>
+							Bowls for food and water
+							</li>
+							<li>
+							Food (dry or canned)
+							</li>
+							<li>
+							Collar
+							</li>
+							<li>
+							4-6 foot leash with an ID tag containing your phone number
+							</li>
+							<li>
+							Foldable metal container or hard plastic carrier
+							</li>
+							<li>
+							Dog bed
+							</li>
+							<li>
+							Shampoo and conditioner for dogs
+							</li>
+							<li>
+							Clippers for nails
+							</li>
+							<li>
+							Toothbrush and toothpaste for dogs
+							</li>
+							<li>
+							Depending on the type and length of your pet's coat, a brush or comb.
+							</li>
+							<li>
+							Paper towels
+							</li>
+							<li>
+							A scrub brush and sponge
+							</li>
+							<li>
+							Non-corrosive cleaner
+							</li>
+							<li>
+							Enzymatic remover of odors
+							</li>
+							<li>
+							Poop baggies made of plastic (the better ones are biodegradable) or a pooper scooper
+							</li>
+							<li>
+							Pads for house training that absorb
+							</li>
+							<li>
+							A variety of toys (a puzzle toy, ball, rope, and chew toy are good places to start)
+							</li>
+							<li>
+							A range of sweets (bigger rawhides, smaller cookies, etc.)
+							</li>
+							<li>
+							First-aid materials
+							</li>
+							<li>
+							Infant gate(s)
+							</li>
+							
+
+
+
+
+						</ul>
+					<li>
+					<strong>Necessary Items for Cats:</strong>
+					</li>
+						<ul>
+							<li>
+							Bowls for food and water
+							</li>
+							<li>
+							Food (dry or canned)
+							</li>
+							<li>
+							Scooper and litter box
+							</li>
+							<li>
+							Cat litter
+							</li>
+							<li>
+							A phone number-linked ID tag on the collar
+							</li>
+							<li>
+							Stiff plastic carrier
+							</li>
+							<li>
+							Clippers for nails
+							</li>
+							<li>
+							Toothpaste and a toothbrush for cats
+							</li>
+							<li>
+							Depending on the type and length of your cat's coat, a brush or comb.
+							</li>
+							<li>
+							Paper towels
+							</li>
+							<li>
+							A scrub brush and sponge
+							</li>
+							<li>
+							Non-corrosive cleaner
+							</li>
+							<li>
+							Enzymatic remover of odors
+							</li>
+							<li>
+							A selection of toys (a favorite is the catnip toys)
+							</li>
+							<li>
+							First-aid materials
+							</li>
+						</ul>
+					</ul>
+				</div>
+			</div>	
 		
 		</>
 	)
